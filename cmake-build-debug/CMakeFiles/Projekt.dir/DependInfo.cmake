de5eb75d8a4a70fb348a6e9c49369a97c2694cd4
Marcin Hawełka <mhawelka@bluesoft.net.pl>
2019-01-13 13:37:07 +0100

    chyba tez trzeba commitowac

---
+++ cmake-build-debug/CMakeFiles/Projekt.dir/DependInfo.cmake
@@ -0,0 +1,54 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/GUI/Projekt/main.cpp" "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/cmake-build-debug/CMakeFiles/Projekt.dir/GUI/Projekt/main.cpp.o"
+  "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/Projekt/main.cpp" "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/cmake-build-debug/CMakeFiles/Projekt.dir/Projekt/main.cpp.o"
+  "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/User/Model/ACL.cpp" "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/cmake-build-debug/CMakeFiles/Projekt.dir/User/Model/ACL.cpp.o"
+  "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/User/Model/User.cpp" "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/cmake-build-debug/CMakeFiles/Projekt.dir/User/Model/User.cpp.o"
+  "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/User/Repository/FileAclRepository.cpp" "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/cmake-build-debug/CMakeFiles/Projekt.dir/User/Repository/FileAclRepository.cpp.o"
+  "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/User/Repository/FileUserRepository.cpp" "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/cmake-build-debug/CMakeFiles/Projekt.dir/User/Repository/FileUserRepository.cpp.o"
+  "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/User/Service/UserService.cpp" "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/cmake-build-debug/CMakeFiles/Projekt.dir/User/Service/UserService.cpp.o"
+  "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/User/Validator/UsernameValidator.cpp" "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/cmake-build-debug/CMakeFiles/Projekt.dir/User/Validator/UsernameValidator.cpp.o"
+  "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/application/Hall/HallManager.cpp" "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/cmake-build-debug/CMakeFiles/Projekt.dir/application/Hall/HallManager.cpp.o"
+  "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/application/Hall/Model/Hall.cpp" "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/cmake-build-debug/CMakeFiles/Projekt.dir/application/Hall/Model/Hall.cpp.o"
+  "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/application/Hall/Reservation/Reservation.cpp" "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/cmake-build-debug/CMakeFiles/Projekt.dir/application/Hall/Reservation/Reservation.cpp.o"
+  "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/application/Hall/Reservation/ReservationData/ReservationData.cpp" "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/cmake-build-debug/CMakeFiles/Projekt.dir/application/Hall/Reservation/ReservationData/ReservationData.cpp.o"
+  "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/application/Hall/ReservationList/Model/ReservationListElement.cpp" "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/cmake-build-debug/CMakeFiles/Projekt.dir/application/Hall/ReservationList/Model/ReservationListElement.cpp.o"
+  "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/application/Hall/ReservationList/ReservationList.cpp" "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/cmake-build-debug/CMakeFiles/Projekt.dir/application/Hall/ReservationList/ReservationList.cpp.o"
+  "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/application/Hall/Table/Table.cpp" "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/cmake-build-debug/CMakeFiles/Projekt.dir/application/Hall/Table/Table.cpp.o"
+  "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/application/MenuItem/Dish/Dish.cpp" "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/cmake-build-debug/CMakeFiles/Projekt.dir/application/MenuItem/Dish/Dish.cpp.o"
+  "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/application/MenuItem/Drink/Drink.cpp" "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/cmake-build-debug/CMakeFiles/Projekt.dir/application/MenuItem/Drink/Drink.cpp.o"
+  "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/application/MenuItem/MenuItem.cpp" "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/cmake-build-debug/CMakeFiles/Projekt.dir/application/MenuItem/MenuItem.cpp.o"
+  "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/application/Report/Bill.cpp" "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/cmake-build-debug/CMakeFiles/Projekt.dir/application/Report/Bill.cpp.o"
+  "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/application/Report/BufferedStatisticService.cpp" "/cygdrive/c/Prywatne/Kody/Studia/Projekt PO/Projekt/cmake-build-debug/CMakeFiles/Projekt.dir/application/Report/BufferedStatisticService.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../application/Hall"
+  "../application/Hall/Model"
+  "../application/Hall/Reservation"
+  "../application/Hall/Reservation/ReservationData"
+  "../application/Hall/ReservationList"
+  "../application/Hall/ReservationList/Model"
+  "../application/Hall/Table"
+  "../application/MenuItem"
+  "../application/MenuItem/Dish"
+  "../application/MenuItem/Drink"
+  "../User/Exception"
+  "../User/Model"
+  "../User/Repository"
+  "../User/Service"
+  "../User/Validator"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Projekt.dir/cmake_clean.cmake
@@ -0,0 +1,30 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projekt.dir/application/Hall/Model/Hall.cpp.o"
+  "CMakeFiles/Projekt.dir/application/Hall/Reservation/ReservationData/ReservationData.cpp.o"
+  "CMakeFiles/Projekt.dir/application/Hall/Reservation/Reservation.cpp.o"
+  "CMakeFiles/Projekt.dir/application/Hall/ReservationList/Model/ReservationListElement.cpp.o"
+  "CMakeFiles/Projekt.dir/application/Hall/ReservationList/ReservationList.cpp.o"
+  "CMakeFiles/Projekt.dir/application/Hall/Table/Table.cpp.o"
+  "CMakeFiles/Projekt.dir/application/Hall/HallManager.cpp.o"
+  "CMakeFiles/Projekt.dir/application/MenuItem/Dish/Dish.cpp.o"
+  "CMakeFiles/Projekt.dir/application/MenuItem/Drink/Drink.cpp.o"
+  "CMakeFiles/Projekt.dir/application/MenuItem/MenuItem.cpp.o"
+  "CMakeFiles/Projekt.dir/GUI/Projekt/main.cpp.o"
+  "CMakeFiles/Projekt.dir/Projekt/main.cpp.o"
+  "CMakeFiles/Projekt.dir/User/Model/ACL.cpp.o"
+  "CMakeFiles/Projekt.dir/User/Model/User.cpp.o"
+  "CMakeFiles/Projekt.dir/User/Repository/FileAclRepository.cpp.o"
+  "CMakeFiles/Projekt.dir/User/Repository/FileUserRepository.cpp.o"
+  "CMakeFiles/Projekt.dir/User/Service/UserService.cpp.o"
+  "CMakeFiles/Projekt.dir/User/Validator/UsernameValidator.cpp.o"
+  "CMakeFiles/Projekt.dir/application/Report/Bill.cpp.o"
+  "CMakeFiles/Projekt.dir/application/Report/BufferedStatisticService.cpp.o"
+  "Projekt.pdb"
+  "Projekt.exe"
+  "libProjekt.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Projekt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
