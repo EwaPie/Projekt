22ce98e7377a88f6f38532c4eb2420a7c4f2bd23
Marcin Hawełka <mhawelka@bluesoft.net.pl>
2019-01-20 19:44:08 +0100

    Nowa nadzieja

---
+++ JAVA/restauracja/src/main/java/com/projekt/controller/MenuController.java
@@ -0,0 +1,80 @@
+package com.projekt.controller;
+
+import com.projekt.dto.Danie;
+import com.projekt.service.DanieService;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.util.List;
+
+@Named
+@ViewScoped
+public class MenuController {
+
+    private final DanieService danieService;
+
+    @Getter
+    @Setter
+    private Danie noweDanie;
+
+    @Getter
+    @Setter
+    private boolean edycja = false;
+
+    @Getter
+    private List<Danie> dania;
+
+    @Inject
+    public MenuController(DanieService danieService) {
+        this.danieService = danieService;
+        noweDanie = new Danie();
+        refresh();
+    }
+
+    private void refresh() {
+        dania = danieService.pobierzWszystkieDania();
+    }
+
+    public String formatPrice(BigDecimal value) {
+
+        DecimalFormat df = new DecimalFormat();
+
+        df.setMaximumFractionDigits(2);
+
+        df.setMinimumFractionDigits(0);
+
+        df.setGroupingUsed(false);
+
+        return df.format(value);
+    }
+
+    public void dodajDanie() {
+        danieService.dodajDanie(noweDanie);
+        noweDanie = new Danie();
+        if (edycja) {
+            edycja = false;
+        }
+
+        refresh();
+    }
+
+    public void usunDanie(Danie danie) {
+        danieService.usunDanie(danie);
+        refresh();
+    }
+
+    public void edytujDanie(Danie danie) {
+        noweDanie = danie;
+        edycja = true;
+    }
+
+    public void anulujEdycje() {
+        noweDanie = new Danie();
+        edycja = false;
+    }
+}
