54d3e66c915d566b02e40afaa8c8d9cda64544ba
ElderDrop <kij242@gmail.com>
2019-01-22 21:53:58 +0100

    ale fuckup

--- JAVA/restauracja/src/main/java/com/projekt/service/OrderService.java
+++ JAVA/restauracja/src/main/java/com/projekt/service/OrderService.java
@@ -11,17 +11,13 @@ import java.util.List;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final OrderHistoryRepository orderHistoryRepository;
 
     @Inject
-    public OrderService(OrderRepository orderRepository, OrderHistoryRepository orderHistoryRepository) {
+    public OrderService(OrderRepository orderRepository) {
         this.orderRepository = orderRepository;
-        this.orderHistoryRepository = orderHistoryRepository;
     }
 
     public List<Order> getAll(){return orderRepository.getAll();}
 
     public void save(Order newOrder) {this.orderRepository.save(newOrder);}
-
-    public void toHistory(OrderHistory orderHistory) {this.orderHistoryRepository.save(orderHistory);}
 }
